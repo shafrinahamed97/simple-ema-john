dbe91d4a5caba52c90f5c2fa134a3fd7734464ca
shafrinahamed97 <shafrinahamedjob@gmail.com>
2023-04-07 15:52:37 +0600

    shop component layout, load data and set state

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,17 @@
 import React from 'react';
-
+import './Header.css';
 import logo from '../../images/Logo.svg';
 
 const Header = () => {
     return (
-        <nav>
+        <nav className='header'>
             <img src={logo} alt="" />
+            <div>
+                <a href="/shop">Shop</a>
+                <a href="/orders">Orders</a>
+                <a href="/inventory">Inventory</a>
+                <a href="/about">About</a>
+            </div>
         </nav>
     );
 };
