04f7d9a097a69ccb2668495df31d2693a6b7a90b
shafrinahamed97 <shafrinahamedjob@gmail.com>
2023-04-07 15:02:48 +0600

    started this project

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import logo from '../../images/Logo.svg';
+
+const Header = () => {
+    return (
+        <nav>
+            <img src={logo} alt="" />
+        </nav>
+    );
+};
+
+export default Header;
